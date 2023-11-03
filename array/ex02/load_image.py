2ad85dbf0bcfa422f646ab243a23e9079841bdb2
Kemeli <kdnogueira791@gmail.com>
2023-11-03 10:12:48 -0300

    finishes loading image error handling

--- array/ex02/load_image.py
+++ array/ex02/load_image.py
@@ -3,10 +3,13 @@ from PIL import Image
 import numpy as np
 
 def ft_load(path: str):
-	img = Image.open(path)
-	np_data = np.asarray(img)
-	print(np_data.shape)
-	return np_data
-	# print(img.format)
-	# print(img.size)
-	# print(img.mode)
+	try:
+		img = Image.open(path)
+		assert img.format == 'JPG' or img.format == 'JPEG', 'Invalid image format'
+	except Exception as e:
+		print(f'Error loading image: {str(e)}')
+	else:
+		np_data = np.asarray(img)
+		print(np_data.shape)
+		return np_data
+
