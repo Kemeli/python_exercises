82932e729fa4ced2a842883682070d301bb143a1
Kemeli <kdnogueira791@gmail.com>
2023-11-03 10:33:03 -0300

    improves bmi ex error handling

--- array/ex00/give_bmi.py
+++ array/ex00/give_bmi.py
@@ -3,16 +3,15 @@ import numpy as np
 def give_bmi(height: list[int | float], weight: list[int | float]) -> list[int | float]:
 	np_height = np.array(height)
 	np_weight = np.array(weight)
-	lst = []
-	if np_height.size != np_weight.size:
-		print("give_bmi(): height and weight lists should have the same length")
-	else:
-		try:
-			np_squared = np.square(np.array(np_height))
-			bmi = np.divide(np_weight, np_squared)
-			lst = bmi.tolist()
-		except:
-			print("give_bmi(): values should be integers or floats")
+
+	try:
+		assert np_height.size == np_weight.size, \
+			'height and weight lists should have the same length'
+		np_squared = np.square(np.array(np_height))
+		bmi = np.divide(np_weight, np_squared)
+	except Exception as e:
+		print(f"Error: {str(e)}")
+	lst = bmi.tolist()
 	return lst
 
 
