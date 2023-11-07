7b5bbba7b8ace1aaabb743722b67c35b52bceae5
Kemeli <kdnogueira791@gmail.com>
2023-11-07 08:42:12 -0300

    adds error handling to graph ex

--- DataTable/ex01/aff_life.py
+++ DataTable/ex01/aff_life.py
@@ -1,17 +1,20 @@
 import matplotlib.pyplot as plt
 
 def get_graph(df):
-	campus_data = df[df['country'] == 'Brazil']
-	years = campus_data.columns[1:]
-	life_expectancy = campus_data.values[0][1:]
+	try:
+		campus_data = df[df['country'] == 'Brazil']
+		years = campus_data.columns[1:]
+		life_expectancy = campus_data.values[0][1:]
+	except Exception as e:
+		print(f'{type(e).__name__}: {str(e)}')
+		return
+
 	plt.figure(figsize=(10, 6))
 	plt.plot(years, life_expectancy)
 	plt.title('Brazil Life Expectancy projection')
 	plt.xlabel('Year')
 	plt.xticks(years[::40])
 	plt.ylabel('Life Expectancy')
-
 	plt.legend()
-	# plt.grid(True)
 	plt.tight_layout()
 	plt.savefig("graph.png")
