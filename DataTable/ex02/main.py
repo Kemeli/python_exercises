9b31a93dca2f3b96d21ed6e2d1d36697c2a15a9d
Kemeli <kdnogueira791@gmail.com>
2023-11-07 20:13:05 -0300

    uncomment load return

--- DataTable/ex02/main.py
+++ DataTable/ex02/main.py
@@ -3,7 +3,7 @@ from aff_pop import get_graph
 
 def main():
 	loaded = load("population_total.csv")
-	# print(loaded)
+	print(loaded)
 	campus = "France"
 	other = "Belgium"
 	get_graph(loaded, campus, other)
