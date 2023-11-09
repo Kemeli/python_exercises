94f379e8418ba9bfa902d46dc297f75b572a291d
Kemeli <kdnogueira791@gmail.com>
2023-11-09 11:42:36 -0300

    adds operator overloading ex

---
+++ OOP/ex03/ft_calculator.py
@@ -0,0 +1,22 @@
+class calculator:
+	def __init__(self, arr):
+		self.array = arr
+
+	def __add__(self, object) -> None:
+		self.array = [i + object for i in self.array]
+		print(self.array)
+
+	def __mul__(self, object) -> None:
+		self.array = [i * object for i in self.array]
+		print(self.array)
+
+	def __sub__(self, object) -> None:
+		self.array = [i - object for i in self.array]
+		print(self.array)
+
+	def __truediv__(self, object) -> None:
+		try:
+			self.array = [i / object for i in self.array]
+			print(self.array)
+		except Exception as e:
+			print (f'{type(e).__name__} : {str(e)}')
