b61283f94a23d80c1e40d315ec3e4e4a0aeacb0f
Kemeli <kdnogueira791@gmail.com>
2023-11-13 16:13:53 -0300

    adds some docstrings to decorator ex

--- data_oriented_design/ex02/callLimit.py
+++ data_oriented_design/ex02/callLimit.py
@@ -1,16 +1,31 @@
 def callLimit(limit: int):
+	"""Sets the limit of times a function can be called, by returning a
+	decorator to the function.
+	Return: decorator callLimiter.
+	"""
 	count = 0
 
 	def callLimiter(function):
+		"""Decorator:
+		It wraps around the function and tracks how many times the function has
+		been called.
+		Return: wrapper limit_function.
+		"""
 
-		def limit_functionn(*args: any, **kwds: any):
+		def limit_function(*args: any, **kwds: any):
+			"""Wrapper:
+			It limits 'function' calls.
+			Increments 'count' when the wrapped function is called and calls
+			'function' if 'count' hasn't reached it's 'limit';
+			Return: None"""
+
 			nonlocal count
 			count += 1
 			if count > limit:
 				print (f'Error {function} call too many times')
-				return
-			function(*args, **kwds)
+			else:
+				function(*args, **kwds)
 
-		return limit_functionn
+		return limit_function
 	return callLimiter
 
