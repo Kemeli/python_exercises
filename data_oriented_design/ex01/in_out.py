0e4d0034fdb06460f63514e544fd20b260cdf501
Kemeli <kdnogueira791@gmail.com>
2023-11-13 15:10:52 -0300

    adds some docstring to inner function ex

--- data_oriented_design/ex01/in_out.py
+++ data_oriented_design/ex01/in_out.py
@@ -1,13 +1,23 @@
 def square(x: int | float) -> int | float:
+	"""squares a given value"""
+
 	return x ** 2
 
 
 def pow(x: int | float) -> int | float:
+	"""raises a number to its own power"""
+
 	return x ** x
 
 
 def outer(x: int | float, function) -> object:
+	"""returns a function that will do operation in a value updating the value
+	to the result of the previous operation"""
+
 	def inner() -> float:
+		"""calls a function given from the outer function as parameter, while
+		updating 'x' with the result of the previous inner() call"""
+
 		nonlocal x
 		a = function(x)
 		x = a
