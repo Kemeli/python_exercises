2b52dbc06129f1f1af06f672582ffc43d40abd38
Kemeli <kdnogueira791@gmail.com>
2023-11-13 12:01:05 -0300

    adds some ex rules

--- data_oriented_design/ex00/statistics.py
+++ data_oriented_design/ex00/statistics.py
@@ -1,17 +1,20 @@
 def ft_statistics(*args: any, **kwargs: any) -> None:
+	if len(args) == 0:
+		for i in kwargs:
+			print ("ERROR")
+		return
 	try:
 		mean = sum(args) / len(args)
 		lst = [i for i in args]
 		lst.sort()
-		length = len(lst)
-		median = lst[length // 2]
+		median = lst[len(lst) // 2]
 		quartile = []
-		quartile.append(lst[length // 4])
-		quartile.append(lst[length // 4 * 3])
-		variance = sum((mean - x) ** 2 for x in lst) / length
+		quartile.append(lst[len(lst) // 4])
+		quartile.append(lst[len(lst) // 4 * 3])
+		variance = sum((mean - x) ** 2 for x in lst) / len(lst)
 		std_deviation = variance ** 0.5
 	except Exception as e:
-		print('ERROR')
+		print(f'{type(e).__name__}: {e}')
 		return
 
 	dict = {"mean": mean, "median": median, "quartile": quartile,
@@ -20,6 +23,3 @@ def ft_statistics(*args: any, **kwargs: any) -> None:
 	for key in kwargs.values():
 		if key in dict.keys():
 			print(f'{key}: {dict[key]}')
-		else:
-			print('ERROR')
-
